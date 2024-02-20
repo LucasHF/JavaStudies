52aed1875f2512f870fc42895d99eb3b8b4471fe
Lucas Holanda <luks206@gmail.com>
2024-02-20 16:32:19 -0300

    organization

--- src/general/MainClass.java
+++ src/general/MainClass.java
@@ -2,7 +2,36 @@ package general;
 
 import java.util.Scanner;
 
+
 public class MainClass {
+	
+	public void Sum() { //Method to test basic sum
+		int a, b, c;
+		
+		a = 10;
+		b = 11;
+		
+		Scanner entries = new Scanner(System.in);
+		
+		System.out.println("Enter the first number: ");
+		a = entries.nextInt();
+		
+		System.out.println("Enter the second number: ");
+		b = entries.nextInt();
+		
+		c = a+b;
+		
+		System.out.println("The sum of the numbers is: "+c);
+		
+		System.out.println("Enter a new number to sum: ");
+		c += entries.nextInt();
+		
+		System.out.println("The final numbers is: "+c);
+		
+		
+		entries.close();
+		
+	}
 
 	public static void main(String[] args) {
 		int a, b, c;
@@ -28,14 +57,7 @@ public class MainClass {
 			System.out.println("Equal numbers inserted! \n");
 		}
 		
-		/* SIMPLE FUNCTION TO SUM AND ADD UP AT THE END
-		 * System.out.println("The sum of the numbers is: "+c);
-		
-		System.out.println("Enter a new number to sum: ");
-		c += entries.nextInt();*/
-		
 		System.out.println("The final numbers is: "+c);
-		
 		
 		entries.close();
 
