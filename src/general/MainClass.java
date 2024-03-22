8fd958871f2d2b8c0f6221a666e60f9b6ff38f7f
Lucas Holanda <luks206@gmail.com>
2024-03-22 10:12:55 -0300

    testing writing on files

--- src/general/MainClass.java
+++ src/general/MainClass.java
@@ -1,8 +1,12 @@
 package general;
 
 import transporte.TarifaChecker;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -13,7 +17,7 @@ public class MainClass {
 		
 		String cartao, valor, linha, novoCartao, registro;
 		
-		try {
+		/*try {
 		      File relatorio = new File("C:\\Users\\Adm01\\Documents\\Teste passagens antigas.txt"); //encontra o arquivo
 		      Scanner reader = new Scanner(relatorio); //objeto para ler as linhas
 		      TarifaChecker checker = new TarifaChecker(); //objeto para checar a necessidade de modificar o cartao
@@ -37,7 +41,28 @@ public class MainClass {
 		    } catch (FileNotFoundException e) {
 		      System.out.println("An error occurred:");
 		      e.printStackTrace();
-		    }
+		    }*/
+		
+		try {
+			 File relatorio = new File("C:\\Users\\Adm01\\Documents\\ArquivoFinal.txt"); //encontra o arquivo
+			 if(!relatorio.exists()) {
+				 relatorio.createNewFile();
+			 }
+			FileWriter destino = new FileWriter(relatorio);
+			BufferedWriter bfDestino = new BufferedWriter(destino);
+			
+			bfDestino.write("B   373M  57018 171741 732320570041OPERACAO VALIDADOR"
+					+ "            24000101,90000001,900000000121/12/2021 06:15:0221/12/2021 "
+					+ "08:12:100006773300067789I21/12/2021 06:47:3721/12/2021 08:05:23   "
+					+ "00000000,0021/12/202121/12/20210000,00");
+			
+			bfDestino.close();
+			destino.close();
+			
+		}catch (IOException e) {
+		      System.out.println("An error occurred:");
+		      e.printStackTrace();
+		}
 		
 	}
 
